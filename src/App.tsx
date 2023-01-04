8863e9dd349551517c04390ea6ca5d0bd30e75de
Zandra.Alte <zandranicellaalte@gmail.com>
2023-01-04 22:36:27 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,52 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-    // const [latitude, setLatitude] = useState()
-    // const [longitude, setLongitude] = useState()
     const [all, setAll] = useState({
         latitude: "",
         longitude: "",
-    })
-    const [error, setError] = useState({})
+    });
+    const [error, setError] = useState({});
 
     console.log("All", all);
 
-    useEffect(() =>{
-        fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,relativehumidity_2m,windspeed_10m')
-        .then(response => response.json())
-        .then(res => setAll(res))
-        .catch(err => setError(err))
-    }, [])
+    useEffect(() => {
+        fetch(
+            "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,relativehumidity_2m,windspeed_10m"
+        )
+            .then((response) => response.json())
+            .then((res) => setAll(res))
+            .catch((err) => setError(err));
+    }, [error]);
 
     return (
         <div className="App">
             <header className="App-header">
-                <h1>WeatherApp</h1>
-                <div>
-                Latitude {all.latitude} and Longitude {all.longitude}
+                <nav className="App-nav">
+                    <p>About</p>
+                    <p>Contakt</p>
+                </nav>
+                <div className="App-content">
+                    <div>
+                        <h1>Weather App</h1>
+                        <p>Undertext</p>
+                    </div>
+                    <div className="search">
+                        <i className="fa fa-search"></i>
+                        <input
+                            type="text"
+                            className="App-Input"
+                            placeholder="Search"
+                        />
+                    </div>
+                    <button className="App-Button">Submit</button>
+                </div>
+                <div className="App-footer">
+                    Latitude {all.latitude} and Longitude {all.longitude}
                 </div>
             </header>
+            <div className="App-startside">
+                <div></div>
+            </div>
         </div>
     );
 }
